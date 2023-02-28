127c136b889b03de96b26dd9b9b3a9c991003df8
Zyad Bahaa <113908516+ZyadBahaa21@users.noreply.github.com>
2023-03-01 01:49:01 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import Home from "./pages/home/home";
-import Login from "./pages/login/Login";
-import Profile from "./pages/profile/profile";
-import Register from "./pages/register/register";
-
 function App() {
   return <Home />;
 }
